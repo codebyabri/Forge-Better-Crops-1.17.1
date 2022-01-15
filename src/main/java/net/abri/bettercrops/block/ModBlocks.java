223c38da3424d46d90e97851285a8765c5ee1a02
abrisprivatewyldnis <florian.abraham2016@gmail.com>
2022-01-15 11:56:21 +0100

    delete basalt_sand because its not suitable

--- src/main/java/net/abri/bettercrops/block/ModBlocks.java
+++ src/main/java/net/abri/bettercrops/block/ModBlocks.java
@@ -21,11 +21,17 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
 
-    public static final RegistryObject<Block> MUD = registerBlock("mud",
-            () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(0.5f).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> BASALT_SAND = registerBlock("basalt_sand",
+            () -> new Block(BlockBehaviour.Properties.of(Material.SAND).strength(0.5f).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> DRIED_MUD = registerBlock("dried_mud",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(0.5f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> MUD = registerBlock("mud",
+            () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(0.5f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> PACKED_DIRT = registerBlock("packed_dirt",
+            () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(0.5f).requiresCorrectToolForDrops()));
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
